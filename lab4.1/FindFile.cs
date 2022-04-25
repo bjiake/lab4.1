4be608220c5f548b4cc77c2b11a0f63d32b3b9d0
bjiake <suyd@list.ru>
2022-04-25 12:38:26 +0300

    Добавьте файлы проекта.

---
+++ lab4.1/FindFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Directory.EnumerateFiles(catalog, fileName, SearchOption);
+ * catalog (строковый тип string) — путь к каталогу в котором будет производиться поиск. Регистр символов не учитывается.
+ * fileName (строковый тип string) — имя файла для поиска. Оно может содержать подстановочные поисковые символы: * и ?. 
+Звездочка обозначает произвольное количество символов на её месте, а знак вопроса — один символ на месте подстановки.
+ * SearchOption — это перечисление задающее опции поиска. Имеет два значения: SearchOption.TopDirectoryOnly (поиск будет выполнен только в текущем каталоге) и SearchOption.
+AllDirectories (поиск будет произведён в указанном каталоге и во всех его подкаталогах).
+ */
+namespace lab4._1
+{
+    class FindFile
+    {
+        string NameFile;
+        string catalog = @"C:\Users\Блэйк\source\repos\4\FilesForTask";
+        public void Find()
+        {
+            Console.WriteLine("Введите название текстового файла, которого хотите найти:");
+            NameFile = Console.ReadLine();
+            NameFile = NameFile + ".txt";
+            //Directory.EnumerateFiles(catalog, NameFile, SearchOption.AllDirectories);
+            foreach (string findedFile in Directory.EnumerateFiles(catalog, NameFile, SearchOption.AllDirectories))
+            {
+                FileInfo FI;
+                try
+                {
+                    //по полному пути к файлу создаём объект класса FileInfo
+                    FI = new FileInfo(findedFile);
+                    //найденный результат выводим в консоль (имя, путь, размер, дата создания файла)
+                    Console.WriteLine(FI.Name + " " + FI.FullName + " " + FI.Length + "_байт" + " Создан: " + FI.CreationTime);
+
+                }
+                catch //слишком длинное имя файла
+                {
+                    continue;
+                }
+
+            }
+        }
+    }
+}
---
+++ lab4.1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace lab4._1
+{
+    public interface IOriginator
+    {
+        object GetMemento();
+        void SetMemento(object Memento);
+    }
+
+    class FileMemento
+    {
+        public string FirstName;
+        public string LastName;
+    }
+
+    [Serializable]
+    public class Patient : IOriginator
+    {
+        public string _FirstName { get; set; }
+        public string _LastName { get; set; }
+
+        public Patient(string FirstName, string LastName)
+        {
+            _FirstName = FirstName;
+            _LastName = LastName;
+        }
+
+        public object GetMemento()
+        {
+            return new FileMemento
+            {
+                FirstName = this._FirstName,
+                LastName = this._LastName,
+            };
+        }
+
+        public void SetMemento(object Memento)
+        {
+            if (Memento is FileMemento)
+            {
+                var Mem = Memento as FileMemento;
+                _FirstName = Mem.FirstName;
+                _LastName = Mem.LastName;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patient pt = new Patient("John", "Doen");
+
+            FileStream stream = File.Create("C:/Users/Блэйк/source/repos/lab4.1/test.dat");
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            //Сериализация
+            formatter.Serialize(stream, pt);
+            stream.Close();
+
+            //Десериализация
+            stream = File.OpenRead("C:/Users/Блэйк/source/repos/lab4.1/test.dat");
+
+            pt = formatter.Deserialize(stream) as Patient;
+
+            Console.WriteLine("Имя    : " + pt._FirstName);
+            Console.WriteLine("Фамилия: " + pt._LastName);
+            stream.Close();
+            Console.ReadKey();
+
+            //Поиск файла
+            FindFile Find = new();
+            Find.Find();
+
+            Console.Write("Enter text to search: ");
+            string Searchable = Console.ReadLine();
+
+            Patient FirstFile = new Patient("Karl", "Ignat");
+            Patient SecondFile = new Patient("Artem", "Suzuki");
+            Patient ThirdFile = new Patient("Asren", "Arseni");
+
+            List<Patient> ListOfFiles = new List<Patient>
+            {
+                FirstFile, SecondFile, ThirdFile
+            };
+
+            foreach (var File in ListOfFiles)
+            {
+                if (File._FirstName.Contains(Searchable))
+                {
+                    Console.WriteLine($"{Searchable} in {File._LastName}");
+                }
+                else
+                {
+                    Console.WriteLine($"{File._LastName} does`t contains {Searchable}");
+                }
+            }
+            /*
+            var serializer = new XmlSerializer(typeof(Patient));
+
+            using (var writer = new StreamWriter("patients.xml"))
+            {
+                serializer.Serialize(writer, patient);
+            }
+            */
+        }
+    }
+}
